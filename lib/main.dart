b78bc9120b20d732ec7cd0edf31fb8a1fca3e097
Utpal Tiwari <utpaltiwari02@gmail.com>
2023-11-14 12:03:43 +0530

    Third Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:varta_app/services/auth/auth_gate.dart';
+import 'firebase_options.dart';
 // import 'package:varta_app/pages/register_page.dart';
-import 'package:varta_app/services/auth/login_or_register.dart';
+// import 'package:varta_app/services/auth/login_or_register.dart';
 // import 'pages/login_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -14,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginOrRegister(),
+      home: AuthGate(),
     );
   }
 }
